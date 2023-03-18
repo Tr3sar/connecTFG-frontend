1759ad7f3229549cff0f56cc3cd324158dd2f2ca
tr3sar <josepcdg@gmail.com>
2023-03-18 12:06:37 +0100

    modelos de user y message

---
+++ src/app/core/model/User.ts
@@ -0,0 +1,17 @@
+export class User{
+    id: number;
+    name: string;
+    surname: string;
+    email: string;
+    password: string;
+    img_url: string;
+    tfg_url: string;
+    description: string;
+    university_id: number;
+    degree: string;
+    rol: string;
+    status: string;
+    post_id: number[];
+    notification_id: number[]
+    group_id: number[];
+}
