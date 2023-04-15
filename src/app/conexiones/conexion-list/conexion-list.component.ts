349f7f9b8a08333711cca0fb2ecb0cb4b738b1ac
tr3sar <josepcdg@gmail.com>
2023-04-15 10:45:04 +0200

    enviar mensajes al aceptar/rechazar conexion

--- src/app/conexiones/conexion-list/conexion-list.component.ts
+++ src/app/conexiones/conexion-list/conexion-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/core/model/User';
 import { UserService } from 'src/app/core/services/user/user.service';
 import { PublicationService } from 'src/app/feed/publication.service';
+import { LoginService } from 'src/app/login/login.service';
+import { NotificationService } from 'src/app/notifications/notification.service';
 
 @Component({
   selector: 'app-conexion-list',
@@ -13,10 +15,10 @@ export class ConexionListComponent implements OnInit {
   applicants: User[] = [];
   userConections: number[] = [];
 
-  constructor(private publicationService: PublicationService, private userService: UserService) { }
+  constructor(private publicationService: PublicationService, private userService: UserService,
+              private notificationService: NotificationService, private loginService: LoginService) {}
 
   ngOnInit(): void {
-
     this.userService.getUserConections().subscribe(
       conections => {
         this.userConections = conections;
@@ -34,12 +36,25 @@ export class ConexionListComponent implements OnInit {
     this.userService.acceptUserConection(id).subscribe(
       res => this.ngOnInit()
     );
+
+    this.notificationService.createNotification(
+      `El usuario ${this.loginService.getActiveUser().name} ${this.loginService.getActiveUser().surname}
+       ha aceptado tu solicitud de conexión! Ahora ya puedes hablar con él.`,
+      id
+    ).subscribe()
   }
 
   onRejected(id: number) {
     this.publicationService.rejectApplicant(id).subscribe(
       res => this.ngOnInit()
     )
+
+    //Enviar notificacion si te rechazan ?????? xd
+    this.notificationService.createNotification(
+      `El usuario ${this.loginService.getActiveUser().name} ${this.loginService.getActiveUser().surname}
+       ha aceptado tu solicitud de conexión! Ahora ya puedes hablar con él.`,
+      id
+    ).subscribe()
   }
 
 }
