289a7cd956e1cf9dcf0b4e39664c5288e2e6f094
MatiasFraga <matiasfr@outlook.com>
2023-04-27 23:25:44 +0200

    Publication Refactoring - CSS was needed

--- src/app/feed/feed.module.ts
+++ src/app/feed/feed.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublicationListComponent } from './publication-list/publication-list.component';
 import { MatCardModule } from '@angular/material/card';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { HttpClientModule } from '@angular/common/http';
 import { PublicationSaveComponent } from './publication-save/publication-save.component';
@@ -12,17 +12,21 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { PublicationCommentComponent } from './publication-comment/publication-comment.component';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
 @NgModule({
   declarations: [
     PublicationListComponent,
-    PublicationSaveComponent
+    PublicationSaveComponent,
+    PublicationCommentComponent
   ],
   imports: [
     CommonModule,
     MatCardModule,
+    MatIconModule,
     MatButtonToggleModule,
     FormsModule,
     MatPaginatorModule,
